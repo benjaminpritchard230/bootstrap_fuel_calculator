60a3d640ddc03abe8ba0c1c2f26428aa8e5bfc18
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-07-16 20:17:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -32,8 +32,74 @@ const dieselClick = () => {
   document.getElementById("dropdownMenu2").innerHTML = "Diesel";
 };
 
-// Drop down shit
-const petrolButton = document.getElementById("petrol");
-const dieselButton = document.getElementById("diesel");
-petrolButton.addEventListener("click", petrolClick);
-dieselButton.addEventListener("click", dieselClick);
+// // Drop down shit
+// const petrolButton = document.getElementById("petrol");
+// const dieselButton = document.getElementById("diesel");
+// petrolButton.addEventListener("click", petrolClick);
+// dieselButton.addEventListener("click", dieselClick);
+
+// Fuels
+let fuelsDropDown = document.getElementById("fuels");
+
+function Fuel(name, price) {
+  this.name = name;
+  this.price = price;
+  this.showDetails = function () {
+    document.getElementById("price").value = price;
+    document.getElementById("dropdownMenu2").innerHTML = name;
+  };
+  this.createButton = function () {
+    const button = document.createElement("button");
+    button.classList.add("dropdown-item");
+    button.innerHTML = this.name;
+    button.addEventListener("click", this.showDetails);
+    fuelsDropDown.append(button);
+  };
+  this.createButton()
+}
+
+// Define fuels
+let petrol = new Fuel("Petrol", 191);
+let diesel = new Fuel("Diesel", 199);
+let lpg = new Fuel("LPG", 85)
+let noFuel = new Fuel("Select fuel")
+
+
+// Cars
+let carsDropDown = document.getElementById("cars");
+
+function Car(name, mileage, fuel) {
+  this.name = name;
+  this.mileage = mileage;
+  this.fuel = fuel
+  this.showDetails = function () {
+    document.getElementById("mpg").value = mileage;
+    document.getElementById("dropdownMenu3").innerHTML = name;
+    if (fuel == "petrol") {
+      petrol.showDetails()
+    } else if (fuel == "diesel") {
+      diesel.showDetails()
+    } else if (fuel == "lpg") {
+      lpg.showDetails()
+    } else if (fuel == "") {
+      noFuel.showDetails()
+    }
+  };
+  this.createButton = function () {
+    const button = document.createElement("button");
+    button.classList.add("dropdown-item");
+    button.innerHTML = this.name;
+    button.addEventListener("click", this.showDetails);
+    carsDropDown.append(button);
+  };
+  this.createButton()
+}
+
+// Define cars
+let fordFiesta = new Car("Ford Fiesta", 40, "petrol");
+let vwGolf = new Car("VW Golf", 39, "diesel");
+let vwPolo = new Car("VW Polo", 45, "lpg");
+let skodaFabia = new Car("Skoda Fabia", 50, "petrol")
+let none = new Car("Select car", "", "")
+
+
